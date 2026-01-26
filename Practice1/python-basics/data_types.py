a9193b93f641d68f753beed82e7371be0979910d
Kaisar Kadyrov <ka_kadyrov@kbtu.kz>
2026-01-26 23:26:37 +0500

    Data Types

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,19 @@
+#1
+x = 5
+print(type(x))
+
+#2
+x = ["apple", "banana", "cherry"]
+print(x)
+
+#3
+x = range(6)
+print(x)
+
+#4
+x = {"name" : "John", "age" : 36}
+print(x)
+
+#5
+x = True
+print(x)
