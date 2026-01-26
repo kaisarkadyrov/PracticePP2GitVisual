697de1a1d6ed4e23d28d551f9b3ef841483865e5
Kaisar Kadyrov <ka_kadyrov@kbtu.kz>
2026-01-26 23:32:30 +0500

    Strings

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,21 @@
+#1
+a = "Hello"
+print(a)
+
+#2
+for x in "banana":
+  print(x)
+
+#3
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#4
+b = "Hello, World!"
+print(b[2:])
+
+#5
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
