c2b09cdfa0b108341a80ba64f40f6fd971d2a3ca
Kaisar Kadyrov <ka_kadyrov@kbtu.kz>
2026-03-31 11:31:30 +0500

    Change in function

--- Practice8/functions.sql
+++ Practice8/functions.sql
@@ -1,4 +1,3 @@
--- Сначала удаляем старые функции/процедуры
 DROP FUNCTION IF EXISTS get_contacts_by_pattern(text);
 DROP FUNCTION IF EXISTS get_contacts_paginated(integer, integer);
 DROP PROCEDURE IF EXISTS upsert_contact(text, text);
@@ -22,42 +21,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION get_contacts_paginated(p_limit integer, p_offset integer)
+CREATE OR REPLACE FUNCTION get_contacts_paginated(p_limit integer, p_offset integer) # offset сколько записей пропустить
 RETURNS TABLE(id integer, name varchar, phone varchar)
 AS $$
-BEGIN
+BEGIN   
     RETURN QUERY
     SELECT * FROM contacts
     ORDER BY id
     LIMIT p_limit OFFSET p_offset;
 END;
 $$ LANGUAGE plpgsql;
-
--- Процедуры
-CREATE OR REPLACE PROCEDURE upsert_contact(p_name text, p_phone text)
-LANGUAGE plpgsql AS $$
-BEGIN
-    INSERT INTO contacts(name, phone)
-    VALUES(p_name, p_phone)
-    ON CONFLICT(phone) DO UPDATE
-    SET name = EXCLUDED.name;
-END;
-$$;
-
-CREATE OR REPLACE PROCEDURE bulk_upsert_contacts(p_names text[], p_phones text[])
-LANGUAGE plpgsql AS $$
-DECLARE
-    i integer;
-BEGIN
-    FOR i IN array_lower(p_names,1)..array_upper(p_names,1) LOOP
-        CALL upsert_contact(p_names[i], p_phones[i]);
-    END LOOP;
-END;
-$$;
-
-CREATE OR REPLACE PROCEDURE delete_contact(p_value text)
-LANGUAGE plpgsql AS $$
-BEGIN
-    DELETE FROM contacts WHERE name = p_value OR phone = p_value;
-END;
-$$;
